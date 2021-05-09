51b2748adf6c8a4f2862ea26747ebd384d81d78b
Jan Henner <mail@janhenner.de>
2021-05-09 10:53:11 +0200

    Cleaning the code

--- sparkify_app/wrangle_graphs.py
+++ sparkify_app/wrangle_graphs.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import plotly.graph_objs as go
-#import plotly.express as px
 import joblib
 
 def return_graphs(df):
@@ -13,10 +12,6 @@ def return_graphs(df):
     """
 
     # extract data for visuals
-    #sb.countplot(x='gender', data=df);
-
-    #categories = df.iloc[:,4:40].sum().sort_values(ascending=False)
-    #categories_names = categories.index.to_series().apply(lambda x: x.replace('_', ' '))
     gender_count = df.groupby('gender').count().userId
     gender_names = list(gender_count.index)
     hovertext = 'UserId ' + df.userId.apply(str)
@@ -43,41 +38,10 @@ def return_graphs(df):
                 xaxis = dict(title = 'Songs listened per day'),
                 yaxis = dict(title = 'Number of days in month listened'),
                 )
-    # graph_two = []
-    # graph_two.append(
-    #   go.Bar(
-    #   x = genre_names,
-    #   y = genre_counts,
-    #   )
-    # )
-    # layout_two = dict(title = 'Distribution of Message Genres',
-    #             xaxis = dict(title = 'Genre'),
-    #             yaxis = dict(title = 'Count'),
-    #             )
-    #
-    # graph_three = []
-    # # https://stackoverflow.com/questions/57639371/plotly-scatter-isnt-displayed-by-flask
-    # graph_three = go.Figure()
-    # graph_three.add_trace(go.Scatter(
-    #     x = scatter.index[scatter.type=='including stopwords'],
-    #     y = scatter.message[scatter.type=='including stopwords'],
-    #     name = 'including stopwords',
-    #     mode = 'markers'
-    # ))
-    # graph_three.add_trace(go.Scatter(
-    #     x = scatter.index[scatter.type=='without stopwords'],
-    #     y = scatter.message[scatter.type=='without stopwords'],
-    #     name = 'without stopwords',
-    #     mode = 'markers'
-    # ))
-    # graph_three.update_layout(title='Word count per message',
-    #     xaxis_title='Words per Message', yaxis_title='Count')
-    # layout_three = ()
 
     # append all charts to the figures list
     graphs = []
     graphs.append(dict(data=graph_one, layout=layout_one))
     graphs.append(dict(data=graph_two, layout=layout_two))
-    #graphs.append(dict(data=graph_three, layout=layout_three))
 
     return graphs
